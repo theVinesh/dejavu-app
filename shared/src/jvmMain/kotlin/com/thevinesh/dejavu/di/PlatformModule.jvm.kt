1a0a2df092605f9966fd7a28dd99e7de6a042abd
Vinesh Raju Radhakrishnan <vinesh-raju.radhakrishnan@daa.ie>
2026-07-31 12:19:08 +0000

    Add JVM platform stubs and arm64 iOS CI destination
    Provide expect/actual SoundPlayer wiring for jvmTest, and select an
    arm64 iPhone simulator so Xcode no longer requests x86_64.

---
+++ shared/src/jvmMain/kotlin/com/thevinesh/dejavu/di/PlatformModule.jvm.kt
@@ -0,0 +1,9 @@
+package com.thevinesh.dejavu.di
+
+import com.thevinesh.dejavu.platform.JvmSoundPlayer
+import com.thevinesh.dejavu.platform.SoundPlayer
+import org.koin.dsl.module
+
+actual fun platformModule() = module {
+    single<SoundPlayer> { JvmSoundPlayer() }
+}
---
+++ shared/src/jvmMain/kotlin/com/thevinesh/dejavu/platform/SoundPlayer.jvm.kt
@@ -0,0 +1,6 @@
+package com.thevinesh.dejavu.platform
+
+class JvmSoundPlayer : SoundPlayer {
+    override fun playBounce() = Unit
+    override fun release() = Unit
+}
