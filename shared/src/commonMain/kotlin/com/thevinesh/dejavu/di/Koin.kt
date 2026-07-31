24f4aeda17776fa844602807d998fa82a7404148
Vinesh Raju Radhakrishnan <vinesh-raju.radhakrishnan@daa.ie>
2026-07-31 12:26:42 +0000

    Fix iOS Koin init for Swift interop
    Remove the defaulted KoinAppDeclaration parameter so Swift can call
    initKoin() without arguments, and pin the simulator destination to arm64.

--- shared/src/commonMain/kotlin/com/thevinesh/dejavu/di/Koin.kt
+++ shared/src/commonMain/kotlin/com/thevinesh/dejavu/di/Koin.kt
@@ -9,7 +9,6 @@ import org.koin.core.context.startKoin
 import org.koin.core.module.Module
 import org.koin.core.module.dsl.singleOf
 import org.koin.core.module.dsl.viewModelOf
-import org.koin.dsl.KoinAppDeclaration
 import org.koin.dsl.module
 
 val appModule = module {
@@ -22,9 +21,8 @@ val appModule = module {
 
 expect fun platformModule(): Module
 
-fun initKoin(appDeclaration: KoinAppDeclaration = {}) {
+fun initKoin() {
     startKoin {
-        appDeclaration()
         modules(appModule, platformModule())
     }
 }
